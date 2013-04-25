d13f7ce456a2b4dc1fc87376ec389997838fc9d2
Roberto Teixeira <robteix@robteix.com>
2013-04-25 12:17:50 -0300

    Fixed field typo

--- adn.go
+++ adn.go
@@ -64,7 +64,7 @@ func adnVerify(w http.ResponseWriter, r *http.Request) {
 	user := loadUser(r, id)
 	user.ADNAccessToken = tok.AccessToken
 	user.ADNId = adnTok.Data.User.Id
-	user.ADNScreenName = adnTok.Data.User.Username
+	user.ADNScreenName = adnTok.Data.User.UserName
 	if err := saveUser(r, &user); err != nil {
 		serveError(c, w, err)
 		return
